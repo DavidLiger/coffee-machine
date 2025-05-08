803284a0ba05a5de243e9a87ae3965edf5b860c5
David LIGER <liger@biotrade.fr>
2025-05-08 12:12:43 +0200

    modification entity CoffeOrder & ajout enum CoffeStatus

--- api/src/Entity/CoffeeOrder.php
+++ api/src/Entity/CoffeeOrder.php
@@ -3,9 +3,13 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use App\Enum\CoffeeStatus;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
 #[ApiResource(
     operations: [
         new \ApiPlatform\Metadata\GetCollection(),
@@ -28,6 +32,22 @@ class CoffeeOrder
 
     #[ORM\Column(type: 'string')]
     private string $size;
+
+    #[ORM\Column(type: 'string', enumType: CoffeeStatus::class)]
+    private CoffeeStatus $status = CoffeeStatus::PENDING;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private \DateTime $createdAt;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTime $startedAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTime $endedAt = null;
+
+    #[ORM\Column(type: Types::JSON, nullable: true)]
+    private ?array $stepsLog = null;
+
 
     public function getId(): ?int
     {
@@ -66,6 +86,62 @@ class CoffeeOrder
         $this->size = $size;
         return $this;
     }
+
+    public function getStatus(): CoffeeStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(CoffeeStatus $status): self
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getStartedAt(): ?\DateTime
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(?\DateTime $startedAt): self
+    {
+        $this->startedAt = $startedAt;
+        return $this;
+    }
+
+    public function getEndedAt(): ?\DateTime
+    {
+        return $this->endedAt;
+    }
+
+    public function setEndedAt(?\DateTime $endedAt): self
+    {
+        $this->endedAt = $endedAt;
+        return $this;
+    }
+
+    public function getStepsLog(): ?array
+    {
+        return $this->stepsLog;
+    }
+
+    public function setStepsLog(?array $stepsLog): self
+    {
+        $this->stepsLog = $stepsLog;
+        return $this;
+    }
+
 }
 
 
