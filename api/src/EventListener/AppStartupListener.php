0af1d5112cace49c646f79540184aa26ce7b5a1a
David LIGER <liger@biotrade.fr>
2025-05-08 20:29:21 +0200

    fix: debug listener

--- api/src/EventListener/AppStartupListener.php
+++ api/src/EventListener/AppStartupListener.php
@@ -2,12 +2,18 @@
 
 namespace App\EventListener;
 
+
 use App\Entity\CoffeeProcessState;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class AppStartupListener
+class AppStartupListener implements EventSubscriberInterface
 {
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(private EntityManagerInterface $em) {}
+
+    public function onKernelBoot(KernelEvent $event): void
     {
         $repo = $this->em->getRepository(CoffeeProcessState::class);
 
@@ -18,5 +24,12 @@ class AppStartupListener
             $this->em->persist($state);
             $this->em->flush();
         }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelBoot',
+        ];
     }
 }
